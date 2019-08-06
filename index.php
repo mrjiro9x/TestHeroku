69e2fbf11a96462593d09e94323cd6a60f5d5992
Nguyễn Ngọc Hải <52957823+mrjiro9x@users.noreply.github.com>
2019-08-06 14:23:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Hello Heroku</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Processing</title>
 </head>
 <body>
-	<?php 
-		echo "<h1>Hello Heroku PHP!!!</h1>";
-	?>
+<?php
+    /*
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=dbgch17133' , 'postgres', '1602');
+    echo "Connect Successfully!";
+    */
 
+    $db = parse_url(getenv("DATABASE_URL"));
+    $pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+    //$sql = "SELECT studentname, course FROM registercourse";
+    $sql = "SELECT * FROM registercourse";
+    $stmt = $pdo->prepare($sql);
+    // Thiết lập kiểu dữ liệu trả về
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $resultSet = $stmt->fetchAll();
+?>
+    <ul>
+        <h1>Your Information:</h1>
+        <?php
+        foreach($resultSet as $row)
+            echo '<li>Student Name: '. $row["txtName"] .'</li>';
+            echo '<li>Course: '. $row["cbCourse"] .'</li>';
+            echo '<li>Date of Birth: '. $row["dob"] .'</li>';
+            echo '<li>Gender: '. $row["gender"] .'</li>';
+            echo '<li>Favorite: '. $row["fav"] . '</li>';
+        ?>
+    </ul>  
 </body>
 </html>
