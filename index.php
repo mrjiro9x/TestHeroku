a6756164e6786b7e76e29714b436542a84cd37df
Nguyễn Ngọc Hải <52957823+mrjiro9x@users.noreply.github.com>
2019-08-17 04:45:07 +0700

    a

--- index.php
+++ index.php
@@ -2,43 +2,92 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Processing</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Signup</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-    /*
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=dbgch17133' , 'postgres', '1602');
-    echo "Connect Successfully!";
-    */
+    <form class="formCreate" action="" method="post" enctype="multipart/form-data">
+    <div class="create">
+        <h1>Create A New Account</h1>
+        <table>
+            <tr>
+                <td>Full Name</td>
+                <td><input type="text" name="fullname" placeholder="eg. John Doe" require></td>
+            </tr>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username" placeholder="Enter Username Unique" require></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="Password" name="password" placeholder="*********" require></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" name="email" placeholder="johndoe@example.com"></td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td><input type="radio" name="gender" value="Male">Male<input type="radio" name="gender" value="Female">Female</td>
+            </tr>
+            <tr>
+                <td>Avatar</td>
+                <td><input type="file" name="avatar" value="Chose"></td>
+            </tr>
+        </table>
+        <button type="submit" name="submit" >CREATE</button>
+    </div>
+    <?php
+        if(isset($_FILES['avatar'])){
+            $errors= array();
+            $file_name = $_FILES['avatar']['name'];
+            $file_size = $_FILES['avatar']['size'];
+            $file_tmp = $_FILES['avatar']['tmp_name'];
+            $file_type = $_FILES['avatar']['type'];
+            $tmp = end(explode('.',$_FILES['avatar']['name']));
+            $file_ext = strtolower($tmp);
+            
+            $expensions= array("jpeg","jpg","png");
 
-    $db = parse_url(getenv("DATABASE_URL"));
-    $pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+            if(in_array($file_ext,$expensions)=== false){
+                $errors[]="Chỉ hỗ trợ upload file JPEG hoặc PNG.";
+            }
 
-    //$sql = "SELECT studentname, course FROM registercourse";
-    $sql = "SELECT * FROM registercourse";
-    $stmt = $pdo->prepare($sql);
-    // Thiết lập kiểu dữ liệu trả về
-    $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $stmt->execute();
-    $resultSet = $stmt->fetchAll();
-?>
-    <ul>
-        <h1>Your Information:</h1>
+            if($file_size > 5000000) {
+                $errors[]='Kích thước file không được lớn hơn 5MB';
+            }
+
+            if(empty($errors)==true) {
+                move_uploaded_file($file_tmp,"accounts/".$file_name);
+                echo "Success";
+            }else{
+                print_r($errors);
+            }
+        }
+    ?>
+
+        </form>
         <?php
-        foreach($resultSet as $row)
-            echo '<li>Student Name: '. $row["studentname"] .'</li>';
-            echo '<li>Course: '. $row["course"] .'</li>';
-            echo '<li>Date of Birth: '. $row["dob"] .'</li>';
-            echo '<li>Gender: '. $row["gender"] .'</li>';
-            echo '<li>Favorite: '. $row["fav"] . '</li>';
-        ?>
-    </ul>  
+        include_once "connect.php"; 
+        if(!isset($_POST['submit']))
+        {
+        }
+        else
+        {
+            $fullname = $_POST['fullname'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $email = $_POST['email'];
+            $gender = $_POST['gender'];
+            $avatar = $_POST['avatar'];
+            $sql = "INSERT INTO accounts(
+                id, fullname, username, password, email, gender, avatar)
+                VALUES (null, '$fullname', '$username', '$password', $email'', '$gender', '$avatar');";
+            $pdo->exec($sql);
+            echo 'add Sucessfully';
+        }
+    ?>
 </body>
 </html>
